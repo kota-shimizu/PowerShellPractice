108c04b672802a195ea757a365b021d15a82e37a
kota-shimizu <kohta.s.1118@gmail.com>
2025-09-01 12:01:01 +0900

    add content 'Hello PowerShell'

--- main.ps1
+++ main.ps1
@@ -1,3 +1,4 @@
 Write-Output "==== My First PowerShell Script ===="
 Write-Output "Today: $(Get-Date -Format 'yyyy/MM/dd HH:mm:ss')"
 Write-Output "Radom Number: $(Get-Random -Minimum 1 -Maximum 100)"
+Write-Host "Hello PowerShell"
