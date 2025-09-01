a2c61a4d583ac9082b975583ffc2637f8a40e361
kota-shimizu <kohta.s.1118@gmail.com>
2025-09-01 11:05:04 +0900

    main.ps1スクリプトの追加

---
+++ main.ps1
@@ -0,0 +1,3 @@
+Write-Output "==== My First PowerShell Script ===="
+Write-Output "Today: $(Get-Date -Format 'yyyy/MM/dd HH:mm:ss')"
+Write-Output "Radom Number: $(Get-Random -Minimum 1 -Maximum 100)"
